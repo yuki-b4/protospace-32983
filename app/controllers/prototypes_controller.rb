efe2ee68857b6332a8451cfe5a89b70d887d7c6d
yuki-b4 <75466656+yuki-b4@users.noreply.github.com>
2020-12-23 10:51:19 +0900

    トップページの修正と、未ログイン者の編集ページ遷移時におけるリダイレクトの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show]
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, only: [:edit]
 
   def index
